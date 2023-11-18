e81d92075a08aa85c4c824a5e5a8d653dc8af004
leeedohyun <ldh19738@gmail.com>
2023-11-18 12:25:50 +0900

    test: Validation -> PlayerNumber 테스트 코드 리팩토링

--- src/test/java/baseball/model/PlayerNumberTest.java
+++ src/test/java/baseball/model/PlayerNumberTest.java
@@ -4,29 +4,29 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
-class ValidationTest {
+class PlayerNumberTest {
 
     @Test
     public void 중복된_숫자_입력시_예외_발생() {
-        assertThatThrownBy(() -> Validation.validateInputThreeNumber("144"))
+        assertThatThrownBy(() -> new PlayerNumber("144"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void 입력한_문자열에_문자가_포함되어_있으면_예외_발생() {
-        assertThatThrownBy(() -> Validation.validateInputThreeNumber("14j"))
+        assertThatThrownBy(() -> new PlayerNumber("14j"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void 입력한_숫자_개수가_3개가_넘으면_예외_발생() {
-        assertThatThrownBy(() -> Validation.validateInputThreeNumber("1234"))
+        assertThatThrownBy(() -> new PlayerNumber("1234"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void 입력한_숫자_개수가_3개보다_적으면_예외_발생() {
-        assertThatThrownBy(() -> Validation.validateInputThreeNumber("34"))
+        assertThatThrownBy(() -> new PlayerNumber("34"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -38,7 +38,7 @@ class ValidationTest {
 
     @Test
     public void 입력한_숫자가_1에서_9사이의_숫자가_아니면_예외_발생() {
-        assertThatThrownBy(() -> Validation.validateInputThreeNumber("012"))
+        assertThatThrownBy(() -> new PlayerNumber("012"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
