80492107ed31e310c29248f46522ab3e05a120f6
leeedohyun <ldh19738@gmail.com>
2023-12-16 20:52:20 +0900

    feat: 재시작/종료 예외 기능 구현

--- src/main/java/baseball/model/RetryOrEnd.java
+++ src/main/java/baseball/model/RetryOrEnd.java
@@ -2,8 +2,10 @@ package baseball.model;
 
 public class RetryOrEnd {
 
-
     public boolean canRetry(final int command) {
+        if (command != 1 && command != 2) {
+            throw new IllegalArgumentException();
+        }
         return command == 1;
     }
 }
