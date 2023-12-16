b86a776cf0232244523bc11304a05fa0507a71e0
leeedohyun <ldh19738@gmail.com>
2023-12-16 18:24:11 +0900

    test: 중복되는 부분 제거

--- src/test/java/baseball/model/BaseballGameTest.java
+++ src/test/java/baseball/model/BaseballGameTest.java
@@ -1,34 +1,24 @@
 package baseball.model;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class BaseballGameTest {
 
-    @Test
-    void 원_스트라이크() {
-        // given
-        final List<Integer> inputNumber = List.of(1, 2, 3);
-        final List<Integer> randomNumber = List.of(1, 4, 5);
-
+    @ParameterizedTest
+    @MethodSource("provideStrikeTestData")
+    void 원_스트라이크(List<Integer> inputNumber, List<Integer> randomNumber) {
         // when
         BaseballGame baseballGame = new BaseballGame();
         final int strikeNumber = baseballGame.countStrike(inputNumber, randomNumber);
 
         // then
         Assertions.assertEquals(1, strikeNumber);
-
-        // given
-        final List<Integer> inputNumber2 = List.of(1, 2, 3);
-        final List<Integer> randomNumber2 = List.of(6, 2, 5);
-
-        // when
-        BaseballGame baseballGame2 = new BaseballGame();
-        final int strikeNumber2 = baseballGame2.countStrike(inputNumber2, randomNumber2);
-
-        // then
-        Assertions.assertEquals(1, strikeNumber2);
     }
 
     @Test
@@ -57,5 +47,11 @@ public class BaseballGameTest {
 
         // then
         Assertions.assertEquals(3, strikeNumber);
+    }
+
+    private static Stream<Arguments> provideStrikeTestData() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3), List.of(1, 4, 5)),
+                Arguments.of(List.of(1, 2, 3), List.of(6, 2, 5)));
     }
 }
