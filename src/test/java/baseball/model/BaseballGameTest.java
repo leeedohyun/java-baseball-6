cccb602ab7f79390078ed91467a0f0db90534367
leeedohyun <ldh19738@gmail.com>
2023-12-16 18:43:51 +0900

    test: 볼 세는 기능 테스트

--- src/test/java/baseball/model/BaseballGameTest.java
+++ src/test/java/baseball/model/BaseballGameTest.java
@@ -49,6 +49,59 @@ public class BaseballGameTest {
         Assertions.assertEquals(3, strikeNumber);
     }
 
+    @Test
+    void 원_볼() {
+        // given
+        final List<Integer> inputNumber = List.of(1, 2, 3);
+        final List<Integer> randomNumber = List.of(6, 1, 7);
+
+        // when
+        BaseballGame baseballGame = new BaseballGame();
+        final int ballNumber = baseballGame.countBall(inputNumber, randomNumber);
+
+        // then
+        Assertions.assertEquals(1, ballNumber);
+
+        // given
+        final List<Integer> inputNumber2 = List.of(4, 5, 6);
+        final List<Integer> randomNumber2 = List.of(6, 1, 7);
+
+        // when
+        BaseballGame baseballGame2 = new BaseballGame();
+        final int ballNumber2 = baseballGame2.countBall(inputNumber2, randomNumber2);
+
+        // then
+        Assertions.assertEquals(1, ballNumber2);
+    }
+
+    @Test
+    void 투_볼() {
+        // given
+        final List<Integer> inputNumber = List.of(1, 2, 3);
+        final List<Integer> randomNumber = List.of(6, 1, 2);
+
+        // when
+        BaseballGame baseballGame = new BaseballGame();
+        final int ballNumber = baseballGame.countBall(inputNumber, randomNumber);
+
+        // then
+        Assertions.assertEquals(2, ballNumber);
+    }
+
+    @Test
+    void 쓰리_볼() {
+        // given
+        final List<Integer> inputNumber = List.of(1, 2, 3);
+        final List<Integer> randomNumber = List.of(2, 3, 1);
+
+        // when
+        BaseballGame baseballGame = new BaseballGame();
+        final int ballNumber = baseballGame.countBall(inputNumber, randomNumber);
+
+        // then
+        Assertions.assertEquals(3, ballNumber);
+    }
+
     private static Stream<Arguments> provideStrikeTestData() {
         return Stream.of(
                 Arguments.of(List.of(1, 2, 3), List.of(1, 4, 5)),
