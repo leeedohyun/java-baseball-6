7eb1312f102a340ffe675ed80a0feb0eea72b131
leeedohyun <ldh19738@gmail.com>
2023-12-16 16:04:38 +0900

    test: InputNumber 객체 생성 테스트

--- src/test/java/baseball/model/InputNumberTest.java
+++ src/test/java/baseball/model/InputNumberTest.java
@@ -6,6 +6,29 @@ import org.junit.jupiter.api.Test;
 public class InputNumberTest {
 
     @Test
+    void 서로_다른_3개의_숫자를_입력하면_InputNumber_객체가_생성된다() {
+        // given
+        final String input = "123";
+
+        // when
+        final InputNumber inputNumber = InputNumber.create(input);
+
+        //then
+        Assertions.assertThat(inputNumber.getNumbers()).hasSize(3)
+                .contains(1, 2, 3);
+
+        // given
+        final String input2 = "456";
+
+        // when
+        final InputNumber inputNumber2 = InputNumber.create(input2);
+
+        //then
+        Assertions.assertThat(inputNumber2.getNumbers()).hasSize(3)
+                .contains(4, 5, 6);
+    }
+
+    @Test
     void 서로_다른_3개의_숫자가_아닌_경우_예외가_발생한다() {
         Assertions.assertThatIllegalArgumentException().isThrownBy(() -> InputNumber.create("112"));
         Assertions.assertThatIllegalArgumentException().isThrownBy(() -> InputNumber.create("119"));
