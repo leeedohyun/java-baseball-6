65336db25d055a132b6ab24bddbf76e7a7a786e5
leeedohyun <ldh19738@gmail.com>
2023-12-20 15:12:28 +0900

    test: RandomNumber.create 메서드 파라미터 변경에 따른 테스트 코드 작성

--- src/test/java/baseball/model/BaseballGameTest.java
+++ src/test/java/baseball/model/BaseballGameTest.java
@@ -3,16 +3,29 @@ package baseball.model;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mockito;
 
 public class BaseballGameTest {
 
+    private RandomNumberGenerator mock;
+
+    @BeforeEach
+    void setUp() {
+        mock = Mockito.mock(RandomNumberGenerator.class);
+    }
+
     @ParameterizedTest
     @MethodSource("provideStrikeTestData")
-    void 원_스트라이크(final InputNumber inputNumber, final RandomNumber randomNumber) {
+    void 원_스트라이크(final InputNumber inputNumber, final List<Integer> number) {
+        // given
+        Mockito.when(mock.generate()).thenReturn(number);
+        final RandomNumber randomNumber = RandomNumber.create(mock);
+
         // when
         final BaseballGame baseballGame = new BaseballGame();
         final int strikeNumber = baseballGame.countStrike(inputNumber, randomNumber);
@@ -25,7 +38,8 @@ public class BaseballGameTest {
     void 투_스트라이크() {
         // given
         final InputNumber inputNumber = InputNumber.create("123");
-        final RandomNumber randomNumber = RandomNumber.create(List.of(1, 2, 5));
+        Mockito.when(mock.generate()).thenReturn(List.of(1, 2, 5));
+        final RandomNumber randomNumber = RandomNumber.create(mock);
 
         // when
         final BaseballGame baseballGame = new BaseballGame();
@@ -39,7 +53,8 @@ public class BaseballGameTest {
     void 쓰리_스트라이크() {
         // given
         final InputNumber inputNumber = InputNumber.create("123");
-        final RandomNumber randomNumber = RandomNumber.create(List.of(1, 2, 3));
+        Mockito.when(mock.generate()).thenReturn(List.of(1, 2, 3));
+        final RandomNumber randomNumber = RandomNumber.create(mock);
 
         // when
         final BaseballGame baseballGame = new BaseballGame();
@@ -51,7 +66,11 @@ public class BaseballGameTest {
 
     @ParameterizedTest
     @MethodSource("provideBallTestData")
-    void 원_볼(final InputNumber inputNumber, final RandomNumber randomNumber) {
+    void 원_볼(final InputNumber inputNumber, final List<Integer> number) {
+        //given
+        Mockito.when(mock.generate()).thenReturn(number);
+        final RandomNumber randomNumber = RandomNumber.create(mock);
+
         // when
         final BaseballGame baseballGame = new BaseballGame();
         final int ballNumber = baseballGame.countBall(inputNumber, randomNumber);
@@ -64,7 +83,8 @@ public class BaseballGameTest {
     void 투_볼() {
         // given
         final InputNumber inputNumber = InputNumber.create("123");
-        final RandomNumber randomNumber = RandomNumber.create(List.of(6, 1, 2));
+        Mockito.when(mock.generate()).thenReturn(List.of(6, 1, 2));
+        final RandomNumber randomNumber = RandomNumber.create(mock);
 
         // when
         final BaseballGame baseballGame = new BaseballGame();
@@ -78,7 +98,8 @@ public class BaseballGameTest {
     void 쓰리_볼() {
         // given
         final InputNumber inputNumber = InputNumber.create("123");
-        final RandomNumber randomNumber = RandomNumber.create(List.of(2, 3, 1));
+        Mockito.when(mock.generate()).thenReturn(List.of(2, 3, 1));
+        final RandomNumber randomNumber = RandomNumber.create(mock);
 
         // when
         final BaseballGame baseballGame = new BaseballGame();
@@ -90,7 +111,11 @@ public class BaseballGameTest {
 
     @ParameterizedTest
     @MethodSource("provideNothingTestData")
-    void 낫싱(final InputNumber inputNumber, final RandomNumber randomNumber) {
+    void 낫싱(final InputNumber inputNumber, final List<Integer> number) {
+        // given
+        Mockito.when(mock.generate()).thenReturn(number);
+        final RandomNumber randomNumber = RandomNumber.create(mock);
+
         // when
         final BaseballGame baseballGame = new BaseballGame();
         final int strikeNumber = baseballGame.countStrike(inputNumber, randomNumber);
@@ -104,20 +129,20 @@ public class BaseballGameTest {
 
     private static Stream<Arguments> provideStrikeTestData() {
         return Stream.of(
-                Arguments.of(InputNumber.create("123"), RandomNumber.create(List.of(1, 4, 5))),
-                Arguments.of(InputNumber.create("123"), RandomNumber.create(List.of(6, 2, 5))));
+                Arguments.of(InputNumber.create("123"), List.of(1, 4, 5)),
+                Arguments.of(InputNumber.create("123"), List.of(6, 2, 5)));
     }
 
     private static Stream<Arguments> provideBallTestData() {
         return Stream.of(
-                Arguments.of(InputNumber.create("123"), RandomNumber.create(List.of(6, 1, 7))),
-                Arguments.of(InputNumber.create("456"), RandomNumber.create(List.of(6, 1, 7))));
+                Arguments.of(InputNumber.create("123"), List.of(6, 1, 7)),
+                Arguments.of(InputNumber.create("456"), List.of(6, 1, 7)));
     }
 
     private static Stream<Arguments> provideNothingTestData() {
         return Stream.of(
-                Arguments.of(InputNumber.create("123"), RandomNumber.create(List.of(4, 5, 6))),
-                Arguments.of(InputNumber.create("789"), RandomNumber.create(List.of(1, 3, 5))),
-                Arguments.of(InputNumber.create("623"), RandomNumber.create(List.of(9, 1, 5))));
+                Arguments.of(InputNumber.create("123"), List.of(4, 5, 6)),
+                Arguments.of(InputNumber.create("789"), List.of(1, 3, 5)),
+                Arguments.of(InputNumber.create("623"), List.of(9, 1, 5)));
     }
 }
--- src/test/java/baseball/model/RandomNumberTest.java
+++ src/test/java/baseball/model/RandomNumberTest.java
@@ -3,6 +3,7 @@ package baseball.model;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class RandomNumberTest {
 
@@ -10,9 +11,11 @@ class RandomNumberTest {
     void 랜덤_넘버를_생성한다() {
         // given
         final List<Integer> numbers = List.of(1, 2, 3);
+        final RandomNumberGenerator mock = Mockito.mock(RandomNumberGenerator.class);
+        Mockito.when(mock.generate()).thenReturn(numbers);
 
         // when
-        final RandomNumber randomNumber = RandomNumber.create(numbers);
+        final RandomNumber randomNumber = RandomNumber.create(mock);
 
         // then
         Assertions.assertThat(randomNumber.getNumbers()).hasSize(3)
